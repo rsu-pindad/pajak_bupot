625658eaa15a880d3f07c528e4786898672861f0
rsu-pindad <sautomated01@gmail.com>
2024-09-03 11:20:05 +0700

    add modul import personalia import

--- app/Imports/PersonaliaImport.php
+++ app/Imports/PersonaliaImport.php
@@ -4,17 +4,30 @@ namespace App\Imports;
 
 use App\Models\Personalia;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithUpserts;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PersonaliaImport implements ToModel
+class PersonaliaImport implements ToModel, WithHeadingRow, WithUpserts
 {
+    public function uniqueBy()
+    {
+        return 'nik';
+    }
+
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Personalia([
+            'npp' => $row['npp'],
+            'nik' => $row['nik'],
+            'npwp' => $row['npwp'],
+            'status_ptkp' => $row['status_ptkp'],
+            'email' => $row['email'],
+            'no_hp' => $row['no_hp']
         ]);
     }
 }
