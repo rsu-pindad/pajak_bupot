d55c561dbc970d5a95172f18f8747ef0303ba48e
rsu-pindad <sautomated01@gmail.com>
2024-09-03 11:18:58 +0700

    add personalia modul

--- app/Models/Personalia.php
+++ app/Models/Personalia.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Personalia extends Model
 {
     use HasFactory;
+
+    protected $table = 'personalias';
+
+    protected $fillable = [
+        'npp',
+        'nik',
+        'npwp',
+        'status_ptkp',
+        'email',
+        'no_hp'
+    ];
 }
