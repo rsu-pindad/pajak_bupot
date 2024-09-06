d6f982b3c5d72b53ec52e8ea825d5629f573b134
rsu-pindad <sautomated01@gmail.com>
2024-09-06 13:38:20 +0700

    check point pdf locked

--- app/Http/Controllers/KehadiranController.php
+++ app/Http/Controllers/KehadiranController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Kehadiran;
+use App\Models\Personalia;
+use DevRaeph\PDFPasswordProtect\Facade\PDFPasswordProtect;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Spatie\LaravelPdf\Enums\Format;
+use Spatie\LaravelPdf\Enums\Orientation;
+
+use function Spatie\LaravelPdf\Support\pdf;
 
 class KehadiranController extends Controller
 {
-
     public function __construct(Request $request)
     {
-        if (! $request->hasValidSignature()) {
+        if (!$request->hasValidSignature()) {
             abort(401);
         }
     }
@@ -17,5 +25,31 @@ class KehadiranController extends Controller
     public function view(Request $request)
     {
         // echo $request->user;
+        // return view('slip-kehadiran');
+        $dataKehadiran  = Kehadiran::find($request->user);
+        $dataPersonalia = Personalia::where('npp', $dataKehadiran->npp);
+        $randomText     = Str::random(4);
+        $pdfName        = $dataKehadiran->npp_kehadiran . '_';
+        $pdfName       .= $dataKehadiran->kehadiran_bulan . '_';
+        $pdfName       .= $dataKehadiran->kehadiran_tahun . '_';
+        $pdfName       .= $randomText . '_';
+        $pdfName       .= '.pdf';
+        $pdf            = pdf()
+                              ->view('slip-kehadiran', ['kehadiran' => $dataKehadiran, 'personalia' => $dataPersonalia])
+                              ->orientation(Orientation::Landscape)
+                              ->format(Format::A5)
+                              ->margins(2, 2, 2, 2)
+                              ->disk('public')
+                              ->save($pdfName);
+
+        PDFPasswordProtect::setInputFile($pdfName, 'public')
+            ->setOutputFile('slip_kehadiran/' . $pdfName, 'public')
+            ->setFormat('A5')
+            ->setOwnerPassword('RSUPINDAD2024')
+            ->setPassword('1234')
+            ->secure();
+        Storage::disk('public')->delete($pdfName);
+
+        return Storage::disk('public')->download('slip_kehadiran/' . $pdfName);
     }
 }
---
+++ resources/views/slip-kehadiran.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1">
+
+    <title>Slip Kehadiran</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect"
+          href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap"
+          rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+
+  <body>
+
+    <p>Nama Pegawai : {{ $kehadiran->nama_pegawai }}</p>
+    <p>Nomor Pokok Pegawai : {{ $kehadiran->npp_kehadiran }}</p>
+
+    <div class="grid grid-cols-2 gap-4">
+      <div>
+        <p>A. Komponen Perhitungan Kehadiran</p>
+        <p>Tunjangan Kehadiran : {{ $kehadiran->tunjangan_kehadiran }}</p>
+        <p>Tunjangan Kehadiran : {{ $kehadiran->jumlah_hari_kerja }}</p>
+        <p>Tunjangan Kehadiran : {{ $kehadiran->jumlah_jam_terbuang }}</p>
+        <p>Tunjangan Kehadiran : {{ $kehadiran->jumlah_cuti }}</p>
+      </div>
+      <div>
+        <p>B. Perhitungan Kehadiran</p>
+        <p>Potongan Absensi : {{ $kehadiran->potongan_absensi }}</p>
+        <p>Jumlah Pendapatan : {{ $kehadiran->jumlah_pendapatan }}</p>
+        <p>Pembulatan : {{ $kehadiran->jumlah_pembulatan }}</p>
+        <p>Diterimakan : {{ $kehadiran->jumlah_diterimakan }}</p>
+      </div>
+    </div>
+
+  </body>
+
+</html>
