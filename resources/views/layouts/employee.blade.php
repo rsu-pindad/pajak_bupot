b885102f592c879d65a2cf872472b73674a62d39
rsu-pindad <sautomated01@gmail.com>
2024-09-18 14:46:38 +0700

    fix dialog

--- resources/views/layouts/employee.blade.php
+++ resources/views/layouts/employee.blade.php
@@ -9,6 +9,11 @@
           content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
     <!-- Scripts -->
     <wireui:scripts />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -16,10 +21,10 @@
 
   <body>
     <div class="bg-gray-50 antialiased dark:bg-gray-900">
-      <livewire:layout.flowbite-shell-navbar wire:key="{{ uniqid() }}" />
+      <livewire:layout.flowbite-shell-navbar/>
       
       <!-- Sidebar -->
-      <livewire:layout.flowbite-shell-sidebar wire:key="{{ uniqid() }}" />
+      <livewire:layout.flowbite-shell-sidebar/>
       
       <main class="h-auto p-4 pt-20 md:ml-64">
         <x-wireui-notifications position="top"/>
